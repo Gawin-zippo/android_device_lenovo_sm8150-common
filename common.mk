9722c2ec5a4172c8880ae23098a3ee7b073f80ee
Gawin-zippo <knoxme.zippo@gmail.com>
2021-04-16 13:16:26 +0700

    Legionify- Add FOD components.

--- common.mk
+++ common.mk
@@ -212,7 +212,6 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.fingerprint.xml
 
 
-
 # FM
 PRODUCT_PACKAGES += \
     FM2 \
