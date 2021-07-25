94f01abfe266050010ce25566b920f814315839a
Gawin-zippo <knoxme.zippo@gmail.com>
2021-07-26 04:10:51 +0700

    Fixed typo.

--- common.mk
+++ common.mk
@@ -90,7 +90,7 @@ PRODUCT_PACKAGES += \
     AntHalService-Soong \
     com.dsi.ant@1.0.vendor
 
-# Audio# Audio
+# Audio
 PRODUCT_PACKAGES += \
     android.hardware.audio.service \
     android.hardware.audio@6.0-impl \
@@ -311,7 +311,7 @@ PRODUCT_PACKAGES += \
     libOmxVdec \
     libOmxVenc \
     libOmxVidcCommon \
-    libstagefrighthwtel datasheet, It's 2 Cores CP
+    libstagefrighthw
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/media/media_codecs.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs.xml \
